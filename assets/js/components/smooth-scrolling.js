6aad1692f57c2b87e75b8f32044ee531b8df457a
BigFudge <wardlec90@gmail.com>
2018-11-16 03:24:27 +0100

    Initial commit, first template design.

---
+++ assets/js/components/smooth-scrolling.js
@@ -0,0 +1,16 @@
+(function($) {
+    // Smooth Scrolling
+    $('.site-header a[href*=\\#]:not([href=\\#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top - 55
+                }, 1500);
+                return false;
+            }
+        }
+    });
+})(jQuery);
